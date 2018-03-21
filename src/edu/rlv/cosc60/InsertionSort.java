d1559e735ab594f1ee4edfa15b2255815e836c3d
Russel Villacarlos <russelvillacarlos@gmail.com>
2018-03-21 16:07:48 +0800

    Updated Insertion Sort to accept non comparable array with comparator

--- src/edu/rlv/cosc60/InsertionSort.java
+++ src/edu/rlv/cosc60/InsertionSort.java
@@ -1,5 +1,7 @@
 package edu.rlv.cosc60;
 
+import java.util.Comparator;
+
 /**
  *
  * @author russel
@@ -17,4 +19,18 @@ public class InsertionSort {
             }
         }
     }
+    
+    public static <T> void sort(T L[],Comparator<T> cmp){
+        for(int i = 1; i < L.length; i++){
+            for(int j = i;j > 0;j--){
+                if(cmp.compare(L[j],L[j-1]) >= 0){
+                    break;
+                }
+                T tmp = L[j];
+                L[j] = L[j-1];
+                L[j-1] = tmp;
+            }
+        }
+    }
+    
 }
