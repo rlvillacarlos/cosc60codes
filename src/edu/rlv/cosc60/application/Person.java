358b87525a0411516b11c5c806f46912ff069ee2
Russel Villacarlos <russelvillacarlos@gmail.com>
2018-04-25 13:58:07 +0800

    Overload the hashCode and equals method

--- src/edu/rlv/cosc60/application/Person.java
+++ src/edu/rlv/cosc60/application/Person.java
@@ -1,5 +1,7 @@
 package edu.rlv.cosc60.application;
 
+import java.util.Objects;
+
 /**
  *
  * @author russel
@@ -34,6 +36,41 @@ public class Person {
                                   .append(lastName).append(")")
                                   .toString();
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 83 * hash + this.id;
+        hash = 83 * hash + Objects.hashCode(this.firstName);
+        hash = 83 * hash + Objects.hashCode(this.lastName);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Person other = (Person) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.firstName, other.firstName)) {
+            return false;
+        }
+        if (!Objects.equals(this.lastName, other.lastName)) {
+            return false;
+        }
+        return true;
+    }
+
+    
 
     
 }
